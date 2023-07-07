b07b2176f63f940bd69a16822d62419b57943932
Jaiditya Batra <jaiditya7318@gmail.com>
2023-07-07 08:42:27 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Node from "./Note";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Node />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+function Node() {
+    return (
+        <div className="note">
+            <h1>This is the title</h1>
+            <p>This is the content</p>
+        </div>
+    );
+}
+
+export default Node;
